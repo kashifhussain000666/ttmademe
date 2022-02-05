2b85e116e7011d3f18fd64c13b357c090c2a8882
kashifhussain000666 <80550972+kashifhussain000666@users.noreply.github.com>
2022-02-05 05:25:47 +0500

    make it better

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $AccessToken = 'ba804eec21ab0d2fa2215953bfd4f584c7f68fe3';
 $callback = '123ATXSfe';
 $referrer = 'https://ttmademe.herokuapp.com/';
 $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
- 
+ die("sadas");
   $data = array(
       'pixel_code' => $pixelcode,
       'event' => $event,
