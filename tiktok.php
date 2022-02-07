a430ab2c6dffba8c10affc7b211b277fd79083e6
kashifhussain000666 <80550972+kashifhussain000666@users.noreply.github.com>
2022-02-08 03:54:42 +0500

    make it better

--- tiktok.php
+++ tiktok.php
@@ -86,5 +86,22 @@ $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http
 catch(Exception $e) {
   echo 'Message: ' .$e->getMessage();
 }
+
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+?>
 echo "Success";
+
+
 ?>
